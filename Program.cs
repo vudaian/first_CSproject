966507785bda19aadb6083b4b961df131b21d893
vudaian <mr,vuan24@gmail.com>
2022-06-30 15:14:56 +0700

    firstCS

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vu Dai An!");
+        }
+    }
+}
